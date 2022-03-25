8dd2bac3f9872ce3b011712aa2aa61e268b9e4e1
BindaChem <bindachemjong32@gnail.com>
2022-03-25 15:52:07 +0545

    day5

---
+++ day-4/qsn-1.js
@@ -0,0 +1,17 @@
+const unSortedArray = [5,2,4,3,1]
+/*var firstNum = unSortedArray.shift();
+unSortedArray.push(firstNum);
+
+var secondNum = unSortedArray.shift();
+unSortedArray.push(secondNum);
+
+var thirdNum = unSortedArray.shift();
+unSortedArray.push(thirdNum);
+
+var fourthNum = unSortedArray.shift();
+unSortedArray.push(fourthNum);
+var fifthNum = unSortedArray.indexOf()
+*/
+var firstNum = unSortedArray.shift();
+var secondNum = unSortedArray.shift();
+console.log(unSortedArray) //[1,2,3,4,5]
